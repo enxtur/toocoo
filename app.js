06b3f2ddf710997dae9f9fb289873ff23b8da30d
enxtur <enxtur.mon@gmail.com>
2015-04-24 12:01:09 +0800

    sync

--- app.js
+++ app.js
@@ -17,3 +17,16 @@ app.listen(80);
 //
 // You can manage your new MySQL database by also embedding phpmyadmin.
 // The phpmyadmin username and password will be the same as the MySQL credentials above.
+
+
+
+// MySQL 5.5 database added.  Please make note of these credentials:
+//
+//        Root User: admin8acic8t
+//    Root Password: 5i1FyggKmeGe
+//    Database Name: enxtur
+//
+// Connection URL: mysql://$OPENSHIFT_MYSQL_DB_HOST:$OPENSHIFT_MYSQL_DB_PORT/
+//
+// You can manage your new MySQL database by also embedding phpmyadmin.
+// The phpmyadmin username and password will be the same as the MySQL credentials above.
